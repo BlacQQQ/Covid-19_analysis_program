043b789d96fe9f7bcac374152be262360df8f712
Jacek <jacek.eugeniusz.bialecki@gmail.com>
2023-09-21 20:50:31 +0200

    Code refactoring and fixed a bug related to incorrect setting of the date range display in df index

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from datetime import datetime, date
 from enum import IntEnum
-from pprint import pprint
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
+
+import statistic
+import variables
 
 
 class ProgramMenu(IntEnum):
@@ -22,73 +24,14 @@ class DataMenu(IntEnum):
     custom_time = 7
 
 
-class
-
-
-    def selecting_nationalities_for_statistics(raw_df):
-        nationality_list = list(raw_df["location"].unique())
-        user_input = input("Podaj w jezyku angielskim z jakiej narodowosci maja zostac podane dane: ").capitalize()
-        if user_input in nationality_list:
-            return user_input
-        else:
-            print("Podana nazwa panstwa nie zostala znaleziona w bazie danych, sprobuj wpisac ponownie")
-            selecting_nationalities_for_statistics(raw_df)
-
-
-def menu_of_analysing_data(raw_df, DataMenu):
-    while True:
-        print("1. Ropocznij analize statystyczna dla 1 panstwa")
-        print("2. Rozpocznij analize statystyczna dla 2 panstw (work in progress)")
-        print("3. Powrot")
-        menu = int(input("Wybierz, co chcesz zrobic: "))
-        match menu:
-            case ProgramMenu.exit:
-                exit()
-            case ProgramMenu.statistic_for_1_country:
-                chosen_nat = selecting_nationalities_for_statistics(raw_df)
-                args = selecting_variables(raw_df)
-                start_date = pd.to_datetime(raw_df.index.min()).date()
-                end_date = pd.to_datetime(raw_df.index.max()).date()
-                index_date = date_range(DataMenu, start_date, end_date)
-                if index_date is None:
-                    index_date = start_date
-                return args, chosen_nat, str(index_date)
-            case ProgramMenu.statistic_for_more_country:
-                print("work in progress")
-                menu_of_analysing_data(raw_df, DataMenu)
-
-
-def dict_of_variables():
-    url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-codebook.csv"
-    df = pd.read_csv(url)
-    df_dict = df[["column", "description"]].set_index("column").T.to_dict("list")
-    pprint(df_dict)
-
-
-def selecting_variables(raw_df):
-    list_of_variables = list(raw_df.columns)
-    args = []
-    a = 0
-    try:
-        user_input = int(input("Podaj ile danych chcesz porownac: "))
-        while a < user_input:
-            user_variable = input("Podaj zmienna do analizy: ")
-            if user_variable in list_of_variables:
-                args.append(user_variable)
-                a += 1
-            else:
-                print("Podana zmienna nie jest obslugiwana")
-        return args
-    except ValueError:
-        print("Nie podales cyfry")
-        selecting_variables(raw_df)
-
-
-def statistical_analysis(chosen_nat, args, raw_df, index_date):
-    df = raw_df[args].loc[raw_df["location"].str.contains(chosen_nat)]
-    df2 = df.loc[index_date:str(datetime.now())]
-    df_clear = df2.dropna(how="all")
-    print(df_clear.to_string())
+def selecting_nationalities_for_statistics(raw_df):
+    nationality_list = list(raw_df["location"].unique())
+    user_input = input("Podaj w jezyku angielskim z jakiej narodowosci maja zostac podane dane: ").capitalize()
+    if user_input in nationality_list:
+        return user_input
+    else:
+        print("Podana nazwa panstwa nie zostala znaleziona w bazie danych, sprobuj wpisac ponownie")
+        selecting_nationalities_for_statistics(raw_df)
 
 
 def date_range(DataMenu, start_date, end_date):
@@ -104,13 +47,13 @@ def date_range(DataMenu, start_date, end_date):
         case DataMenu.last_2_weeks:
             return datetime.now() - relativedelta(days=14)
         case DataMenu.last_month:
-            return datetime.now() - relativedelta(month=1)
+            return datetime.now() - relativedelta(months=1)
         case DataMenu.last_3_months:
             return datetime.now() - relativedelta(months=3)
         case DataMenu.last_half_year:
             return datetime.now() - relativedelta(months=6)
         case DataMenu.last_year:
-            return datetime.now() - relativedelta(year=1)
+            return datetime.now() - relativedelta(years=1)
         case DataMenu.all_time:
             return None
         case DataMenu.custom_time:
@@ -125,16 +68,25 @@ def date_range(DataMenu, start_date, end_date):
                     return user_date
 
 
-# def graphs(df_pol, args):
-#     df_pol[args].plot()
-#     user_input = input("Chcesz zapisac wykres? Tak lub nie: ")
-#     if user_input.capitalize() == "Tak":
-#         plt.savefig("fig.jpg", format="jpg")
-#     plt.show()
-#
-#
-# downloading_file.downloading_file_from_github()
 raw_df = pd.read_csv("covid_data.csv", index_col="date")
-args, chosen_nat, index_date = menu_of_analysing_data(raw_df, DataMenu)
-statistical_analysis(chosen_nat, args, raw_df, index_date)
-# graphs(df_nat, args)
+# downloading_file.downloading_file_from_github()
+while True:
+    print("1. Ropocznij analize statystyczna dla 1 panstwa")
+    print("2. Rozpocznij analize statystyczna dla 2 panstw (work in progress)")
+    print("3. Wyjscie")
+    menu = int(input("Wybierz, co chcesz zrobic: "))
+    match menu:
+        case ProgramMenu.exit:
+            exit()
+        case ProgramMenu.statistic_for_1_country:
+            chosen_nat = selecting_nationalities_for_statistics(raw_df)
+            args = variables.selecting_variables(raw_df)
+            start_date = pd.to_datetime(raw_df.index.min()).date()
+            end_date = pd.to_datetime(raw_df.index.max()).date()
+            index_date = date_range(DataMenu, start_date, end_date)
+            if index_date is None:
+                print(index_date)
+                index_date = start_date
+            statistic.statistical_analysis(chosen_nat, args, raw_df, index_date)
+        case ProgramMenu.statistic_for_more_country:
+            print("work in progress")
